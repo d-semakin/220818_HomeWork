5d259eb245384733acbedd4e20c0a78e7ff90c60
d.semakin <89169812633@mail.ru>
2022-08-19 16:27:35 +0300

    Made Ex2

--- Ex2/Program.cs
+++ Ex2/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает на вход три числа 
+// и выдаёт максимальное из этих чисел.
+
+Console.WriteLine("Введите первое число: ");
+int number1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int number2 = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите третье число: ");
+int number3 = int.Parse(Console.ReadLine());
+int max=number1;
+    if (number2 > max)
+    {
+    max=number2;
+    }
+   if (number3 > max)
+    {
+    max=number3;
+    }
+    System.Console.WriteLine($"Максимальное число {max}");
