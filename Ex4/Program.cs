c9ce8734927e9c8fda455351061926798964cb3a
d.semakin <89169812633@mail.ru>
2022-08-19 17:59:54 +0300

    Made Ex4

--- Ex4/Program.cs
+++ Ex4/Program.cs
@@ -1,18 +1,23 @@
-﻿System.Console.WriteLine("Программа покажет все четные числа в диапазоне введеных чисел");
+﻿System.Console.WriteLine("Программа покажет все четные числа в диапазоне введеных чисел.");
 System.Console.WriteLine("Введите число 1");
 int num1 = int.Parse(Console.ReadLine());
 System.Console.WriteLine("Введите число 2");
 int num2 = int.Parse(Console.ReadLine());
-
+        
 if (num2 > num1)
-    for (int i=num1; i <= num2; i++)
-
-    if (i % 2 == 0)
-        System.Console.Write(i+ " ");
-
-else if (num1 > num2);
-    for (int i=num2; i <= num1; i++)
-
-    if (i % 2 == 0)
-        System.Console.Write(i+ " ");
-else System.Console.WriteLine("Диапазон отсутствует");
+    {
+    System.Console.Write($"Четные числа в диапазоне {num1} - {num2}: ");
+    for (int i = num1; i <= num2; i++)
+        if (i % 2 == 0)
+        System.Console.Write(i + " ");
+    }
+    
+else if (num1 > num2)
+    {
+    System.Console.Write($"Четные числа в диапазоне {num2} - {num1}: ");       
+    for (int i = num2; i <= num1; i++)
+        if (i % 2 == 0)
+        System.Console.Write(i + " ");
+    }
+else if (num1 == num2) 
+System.Console.WriteLine("Диапазон отсутствует.");
