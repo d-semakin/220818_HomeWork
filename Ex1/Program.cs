1d1c3fa3456e3a875ee29fc31246e86b99b53060
d.semakin <89169812633@mail.ru>
2022-08-19 15:28:23 +0300

    Add ex1

---
+++ Ex1/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу, которая на вход принимает два числа и выдаёт, 
+// какое число большее, а какое меньшее.
+
+Console.WriteLine("Введите первое число: ");
+int number1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int number2 = int.Parse(Console.ReadLine());
+{
+    if (number1 == number2)
+    {
+        System.Console.WriteLine("Числа равны");
+    }
+    else if (number1 > number2)
+    {
+    System.Console.WriteLine($"Число {number1} больше");
+    }
+    else System.Console.WriteLine($"Число {number2} больше");
+}
