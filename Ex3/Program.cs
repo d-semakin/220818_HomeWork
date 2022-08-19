b8d722fa78099e1c362f5e6c6f3b197b8817e45d
d.semakin <89169812633@mail.ru>
2022-08-19 16:47:22 +0300

    Made ex3

--- Ex3/Program.cs
+++ Ex3/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая на вход принимает число и выдаёт, 
+// является ли число чётным (делится ли оно на два без остатка).
+Console.WriteLine("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+{
+    if (number % 2 == 0)
+
+    System.Console.WriteLine($"Число {number} является четным");
+
+    else System.Console.WriteLine($"Чисто {number} НЕ является четным");
+}
